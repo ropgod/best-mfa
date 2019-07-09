e2ae2a23947e9e159f4e7297e5c5d0818b47dd37
ropgod <52699760+ropgod@users.noreply.github.com>
2019-07-09 19:51:28 +1000

    initial commit
    hi

---
+++ mfa.php
@@ -0,0 +1,22 @@
+<?php
+    $PAGE = "MFA";
+    include("includes/db.php");
+    include("includes/header.php");
+    include("includes/Base2n.php");
+    require_once 'includes/googleauth.php';
+    if(isset($_POST["code"])){
+        $ga = new PHPGangsta_GoogleAuthenticator();
+        $base32 = new Base2n(5, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567', FALSE, TRUE, TRUE);
+        $secret  = $base32->encode($_SESSION["email"]."-".$_SESSION["id"]);
+        $currentTimeSlice = floor((time() / 30);
+        $result = $ga->verifyCode($secret, $_POST["code"], 2);
+        if($result){
+            // pass
+            $_SESSION["login_stage"] = 3;
+            header("Location: dashboard.php");
+        } else {
+            // fail
+            $message = "Invalid code, please try again.";
+        }
+    }
+?>
